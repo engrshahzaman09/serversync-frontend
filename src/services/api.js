e65889444e8a988249ca7f719927fc9b2a54d947
Shah Zaman <engrshahzaman09@gmail.com>
2026-05-24 04:46:29 +0500

    Update API URL to Elastic IP

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
  
 import axios from 'axios';
 
-const API_BASE_URL = 'http://34.224.65.58:8085/api';
+const API_BASE_URL = 'http://52.72.156.170:8085/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
